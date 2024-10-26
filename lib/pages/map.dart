9b86ff954fe1870cad485344dec98660722a57a0
itzortzis <i.n.tzortzis@gmail.com>
2024-10-26 21:26:33 +0300

    Update: showing existing markers on the map

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -17,66 +17,14 @@ class MapPage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MapPage> {
-  Position? _position;
-  LatLng? _currentPosition;
   LatLng? apiPosition;
 
   final ButtonStyle b_style =
         ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
 
-  // Should be placed in a separate file as a service
-  void _getCurrentLocation() async {
-    Position position = await _determinePosition();
-    setState(() {
-      _position = position;
-      _currentPosition = LatLng(_position!.latitude, _position!.longitude);
-      apiPosition = LatLng(_position!.latitude, _position!.longitude);
-      if (_position != null) {
-        customMarkers = <Marker>[];
-        customMarkers.add(buildPin(LatLng(_position!.latitude.toDouble(), _position!.longitude.toDouble())));
-      }
-    });
-  }
+  
 
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the 
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale 
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-    
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately. 
-      return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-    } 
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
-
-  late var customMarkers = <Marker>[];
+  late Future<List<Marker>> customMarkers;
 
   Marker buildPin(LatLng point) => Marker(
         point: point,
@@ -94,41 +42,52 @@ class _MyHomePageState extends State<MapPage> {
         ),
       );
 
-  void addSinglePin(LatLng point) {
-    setState(() {
-      customMarkers = <Marker>[];
-      customMarkers.add(buildPin(point));
-    });
+
+  
+  final String baseUrl = 'http://147.102.160.160:8000/locations/locations/';
+
+  Future<List<Marker>> fetchLatLngPoints() async {
+    try {
+      final uri = Uri.parse(baseUrl).replace(
+        queryParameters: {
+          'status': 'False',
+        },
+      );
+      // final response = await http.get(Uri.parse(baseUrl));
+      final response = await http.get(uri);
+
+      // Check if request was successful
+      if (response.statusCode == 200) {
+        // Decode the JSON data
+        final List<dynamic> data = json.decode(response.body);
+        
+        // Map each item to a Marker using latitude and longitude
+        List<Marker> markers = data.map((item) {
+          final latitude = double.parse(item['latitude']);
+          final longitude = double.parse(item['longitude']);
+          final id = item['id'].toString();  // Assuming 'id' is in the JSON
+          LatLng latLng = LatLng(latitude, longitude);
+          return buildPin(latLng);
+        }).toList();
+
+        return markers;
+        
+        // Map each item to LatLng and return as a List
+        // return data.map((item) => LatLng.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (error) {
+      throw Exception('Failed to connect to the API: $error');
+    }
   }
 
-  Future<void> sendLocation(double latitude, double longitude) async {
-    var url = Uri.parse('http://147.102.160.160:8000/locations/add-location/'); // Update with your API endpoint
-
-    // Create the body of the request with latitude and longitude
-    Map<String, String> body = {
-      "latitude": double.parse(latitude.toStringAsFixed(6)).toString(),
-      "longitude": double.parse(longitude.toStringAsFixed(6)).toString(),
-    };
-
-    try {
-      var response = await http.post(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(body),
-      );
-
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body);
-        print('Success: $data');
-      } else {
-        print('Failed with status code: ${response.statusCode}');
-        print('Error: ${response.body}');
-      }
-    } catch (e) {
-      print('An error occurred: $e');
-    }
+  @override
+  void initState() {
+    super.initState();
+    // Call the fetchMarkers method when the page gets initiated
+    customMarkers = fetchLatLngPoints();
+    
   }
 
   @override
@@ -141,40 +100,46 @@ class _MyHomePageState extends State<MapPage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Expanded(
-            child: FlutterMap(
-              options: MapOptions(
-                initialCenter: const LatLng(37.4835, 21.6479),
-                initialZoom: 12.0,
-                
-                // onTap: (_, p) => setState(() => customMarkers.add(buildPin(p))),
-                onTap: (_, p) => addSinglePin(p),
-                interactionOptions: const InteractionOptions(
-                  flags: ~InteractiveFlag.doubleTapZoom,
-                ),
-              ),
-              children: [
-                TileLayer(
-                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'com.example.app',
-                ),
-                MarkerLayer(
-                  markers: customMarkers,
-                ),
-              ],
+            child: FutureBuilder<List<Marker>>(
+              future: customMarkers,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No markers found'));
+                } else {
+                  // Extract the LatLng points from the snapshot
+                  final List<Marker> markers = snapshot.data!;
+
+                  return FlutterMap(
+                    
+                    options: MapOptions(
+                      initialCenter: const LatLng(37.4835, 21.6479),
+                      initialZoom: 12.0,
+                      
+                      // onTap: (_, p) => setState(() => customMarkers.add(buildPin(p))),
+                      // onTap: (_, p) => addSinglePin(p),
+                      interactionOptions: const InteractionOptions(
+                        flags: ~InteractiveFlag.doubleTapZoom,
+                      ),
+                    ),
+                    children: [
+                      TileLayer(
+                          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          userAgentPackageName: 'com.example.app',
+                      ),
+                      MarkerLayer(
+                        markers: markers,
+                      ),
+                    ],
+                  );
+                }
+              }
             ),
           ),
-          Row(
-            children: [
-              Expanded(
-                flex:3,
-                child: Center(child: customMarkers.isEmpty != true? Text(customMarkers.first.point.latitude.toString()): null)
-              ),
-              Expanded(
-                flex:3,
-                child: Center(child: customMarkers.isEmpty != true? Text(customMarkers.first.point.longitude.toString()): null)
-              )
-            ],
-          ),
+          
           Row(
 
             children: [
@@ -183,21 +148,11 @@ class _MyHomePageState extends State<MapPage> {
                 flex: 6,
                 child: OutlinedButton(
                   style: OutlinedButton.styleFrom(textStyle: const TextStyle(fontSize: 14)),
-                  onPressed: _getCurrentLocation,
-                  child: const Text('Λήψη τοπεθεσίας GPS'),
+                  onPressed: fetchLatLngPoints,
+                  child: const Text('Test'),
                 ),
               ),
-              Expanded(
-                flex: 6,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(textStyle: const TextStyle(fontSize: 14)),
-                  // onPressed: _getCurrentLocation,
-                  onPressed: customMarkers.isEmpty != true ? () {
-                    sendLocation(customMarkers.first.point.latitude, customMarkers.first.point.longitude);
-                  } : null,
-                  child: const Text('Αποστολή θέσης σάκου'),
-                ),
-              ),
+              
               // Expanded(
               //   flex: 3,
               //   child: OutlinedButton(
