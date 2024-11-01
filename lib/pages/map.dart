62eb13aef6bb538a65dbed09a2a4a7c5a2275e7a
itzortzis <itzortzis@mail.ntua.gr>
2024-11-01 04:08:42 +0200

    Update: more efficient way to change color of marker using separate class

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -8,6 +8,73 @@ import 'dart:convert';
 
 import '../services/api.dart';
 
+class CustomMarker extends StatefulWidget {
+  final LatLng point;
+  final Color initialColor;
+  final String bucketInfo;
+  final Function(Color) onColorChange;
+
+  const CustomMarker({
+    Key? key,
+    required this.point,
+    required this.initialColor,
+    required this.bucketInfo,
+    required this.onColorChange,
+  }) : super(key: key);
+
+  @override
+  _CustomMarkerState createState() => _CustomMarkerState();
+}
+
+class _CustomMarkerState extends State<CustomMarker> {
+  late Color markerColor;
+
+  @override
+  void initState() {
+    super.initState();
+    markerColor = widget.initialColor;
+  }
+
+  void toggleColor() {
+    setState(() {
+      if (markerColor == const Color.fromARGB(255, 201, 4, 4)) {
+        markerColor = const Color.fromARGB(255, 21, 13, 253);
+      } else if (markerColor == const Color.fromARGB(255, 21, 13, 253)) {
+        markerColor = const Color.fromARGB(255, 201, 4, 4);
+      }
+          
+    });
+    widget.onColorChange(markerColor);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: toggleColor,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            widget.bucketInfo,
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.white.withOpacity(0.7),
+            ),
+          ),
+          Icon(
+            Icons.location_pin,
+            size: 30,
+            color: markerColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key, required this.title});
@@ -36,66 +103,92 @@ class _MyHomePageState extends State<MapPage> {
     
   }
 
-  void buildMarkers() {
-    setState(() {
-      customMarkers = customMarkers.map((marker) {
-        return buildPin(marker.point);
-      }).toList();
-    });
-    
-  }
+  // void buildMarkers() {
+  //   setState(() {
+  //     customMarkers = customMarkers.map((marker) {
+  //       return buildPin(marker.point);
+  //     }).toList();
+  //   });
 
-  void changeMarkerColor(LatLng point) {
     
-      if (markerColors[point] == const Color.fromARGB(255, 201, 4, 4)) {
-        markerColors[point] = Color.fromARGB(255, 21, 13, 253);
-      } else if (markerColors[point] == const Color.fromARGB(255, 21, 13, 253)) {
-        markerColors[point] = Color.fromARGB(255, 201, 4, 4);
-      }
+  // }
+
+  // void changeMarkerColor(LatLng point) {
+    
+  //     if (markerColors[point] == const Color.fromARGB(255, 201, 4, 4)) {
+  //       markerColors[point] = Color.fromARGB(255, 21, 13, 253);
+  //     } else if (markerColors[point] == const Color.fromARGB(255, 21, 13, 253)) {
+  //       markerColors[point] = Color.fromARGB(255, 201, 4, 4);
+  //     }
       
-      buildMarkers();
+      
+  //     buildMarkers();
     
+  // }
+
+  // Marker buildPin(LatLng point) => Marker(
+  //   point: point,
+  //   width: 60,
+  //   height: 60,
+  //   child: GestureDetector(
+  //     onTap: () {
+        
+  //       changeMarkerColor(point);
+        
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(
+  //           content: Text('Σάκος χρήστης με id 2'),
+  //           duration: Duration(seconds: 1),
+  //           showCloseIcon: true,
+  //         ),
+  //       );
+  //     },
+  //     child: 
+  //       Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: [
+  //           Text(
+  //             '${markerBuckets[point]}',
+  //             style: TextStyle(
+  //               fontSize: 10,
+  //               color: Colors.black,
+  //               fontWeight: FontWeight.bold,
+  //               backgroundColor: Colors.white.withOpacity(0.7),
+  //             ),
+  //           ),
+  //           Icon(
+  //             Icons.location_pin,
+  //             size: 30,
+  //             color: markerColors[point],
+  //           ),
+  //         ],
+  //       ),
+  //     // Icon(Icons.location_pin, size: 30, color: pinColor),
+  //   ),
+  // );
+
+
+  Marker buildPin(LatLng point) {
+    Color color = markerColors[point] ?? const Color.fromARGB(255, 201, 4, 4);
+    String bucketInfo = markerBuckets[point] ?? '';
+
+    return Marker(
+      point: point,
+      width: 60,
+      height: 60,
+      child: CustomMarker(
+        point: point,
+        initialColor: color,
+        bucketInfo: bucketInfo,
+        onColorChange: (Color newColor) {
+          setState(() {
+            markerColors[point] = newColor; // Update the color in the main state
+          });
+        },
+      ),
+    );
   }
 
-  Marker buildPin(LatLng point) => Marker(
-    point: point,
-    width: 60,
-    height: 60,
-    child: GestureDetector(
-      onTap: () {
-        
-        changeMarkerColor(point);
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Σάκος χρήστης με id 2'),
-            duration: Duration(seconds: 1),
-            showCloseIcon: true,
-          ),
-        );
-      },
-      child: 
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '${markerBuckets[point]}',
-              style: TextStyle(
-                fontSize: 10,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                backgroundColor: Colors.white.withOpacity(0.7),
-              ),
-            ),
-            Icon(
-              Icons.location_pin,
-              size: 30,
-              color: markerColors[point],
-            ),
-          ],
-        ),
-      // Icon(Icons.location_pin, size: 30, color: pinColor),
-    ),
-  );
 
   void fetchLatLngPoints() async {
     const String baseUrl = 'http://147.102.160.160:8000/locations/locations/';
